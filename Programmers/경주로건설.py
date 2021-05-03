298be5c95cb0b15bb787f258df51e6088a74ec3b
이은재 <silver_jae@naver.com>
2021-05-03 19:39:21 +0900

    프로그래머스-경주로 건설

--- Programmers/경주로건설.py
+++ Programmers/경주로건설.py
@@ -1,58 +1,56 @@
 '''
-상 <-> 하 or 좌 <-> 우 : 직선도로
-직선도로가 직각으로 만나는지점이 코너
+프로그래머스 - 경주로 건설
+1시간 25분걸림
+
+격자는 1 or 0으로 채워짐
+0 : 비어있음을 의미
+1 : 채워져 있음
+
+직선 도로 : 상 <-> 하 or 좌 <-> 우
+코너 : 직선도로를 제외한 모든 도로
 직선도로 => 100원
 코너 ==> 500원
 
-bfs
-이전의 위치를 함수 인자에 넣자
-예를들면 상하좌우 1,2,3,4로 해서 다음 위치가 3인데 이전의 위치가 1 or 2이면 500원 추가
+최소비용을 찾자!!
 '''
-dx = [0,-1,1,0,0] # 상하좌우
+
+from collections import deque
+
+dx = [0,-1,1,0,0] # 1:상, 2:하, 3: 좌, 4: 우
 dy = [0,0,0,-1,1]
-minV = float('inf')
-def coner(coner,pre_k,k,nx,ny,x,y):
-    # 코너일경우
-    add = coner[x][y]
-    if ((pre_k == 1 or pre_k == 2) and (k == 3 or k == 4)) or ((pre_k == 3 or pre_k == 4) and (k == 1 or k == 2)):
-        if coner[nx][ny] >= coner[x][y] + 600:
-            add += 500
-    # 코너가 아닐경우
-    add += 100
-    return add
-def BFS(board, visited ,n, x, y,res):
-    global minV
-    q = [[x,y,0]]
-    INF = float('inf')
-    coner_list = [[INF] * n for _ in range(n)]
-    visited = [[0]*n for _ in range(n)]
-    visited[0][0] = 1
-    coner_list[0][0] = 0
-    while q:
-        x,y,pre_k = q.pop(0)
-        if x == n-1 and y == n-1:
-            if minV > coner_list[n-1][n-1]:
-                minV = coner_list[n-1][n-1]
-            continue
-        for k in range(1,5):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0<=nx<n and 0<=ny<n and not board[nx][ny]:
-                add = coner(coner_list,pre_k,k,nx,ny,x,y)
-                if not visited[nx][ny] or (coner_list[nx][ny] >= add):
-                    coner_list[nx][ny] = add
-                    visited[nx][ny] = 1
-                    q.append([nx,ny,k])
-    return minV
+passD = [0,2,1,4,3]
 
 def solution(board):
-    n = len(board)
-    visited = [[0]*n for _ in range(n)]
-    visited[0][0] = 1
-    answer = BFS(board, visited, n, 0,0,0)
-    return answer
+    N = len(board)
+    q = deque()
+    # x좌표, y좌료, 방향, 비용 순서
+    visited = {(0,0,1) : 0, (0,0,2) : 0, (0,0,3) : 0,(0,0,4) : 0}
+    q.append([0,0,-1,0])
+    minValue = float('inf')
+
+    while q:
+        x, y, preD, cost = q.popleft()
+        for i in range(1,5):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N and not board[nx][ny]:
+                if preD == -1:
+                    nextCost = cost + 100
+                elif preD == i:
+                    nextCost = cost + 100
+                else:
+                    nextCost = cost + 600
+
+                if x == N - 1 and y == N - 1:
+                    minValue = min(minValue, cost)
+
+                elif visited.get((nx,ny,i)) is None or visited.get((nx,ny,i)) > nextCost:     # 방문을 전혀안했다면 방문
+                    visited[(nx,ny,i)] = nextCost
+                    q.append((nx,ny,i,nextCost))
+    return minValue
 
 
-board =[[0,0,0],[0,0,0],[0,0,0]]
-# board = [[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]
-solution(board)
+# board =[[0,0,0],[0,0,0],[0,0,0]]
+board = [[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]]
+# board = [[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]]
+print(solution(board))
