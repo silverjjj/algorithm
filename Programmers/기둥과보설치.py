f6d572bc0110ffe75f057d225c3cdec38f62c859
이은재 <silver_jae@naver.com>
2021-05-07 21:46:28 +0900

    프로그래머스-외벽점검

--- Programmers/기둥과보설치.py
+++ Programmers/기둥과보설치.py
@@ -7,60 +7,15 @@ x,y,a,b
 x,y : 좌표
 a : 0 기둥, 1 보
 b : 0 삭제, 1 설치
+
+출력값 [x,y,a]
+x,y : 좌표
+a : 0은 기둥 1은 보
+x기준으로 오른차순, y기준으로 오름 차순 ,a 기준으로 오름차순
+
 '''
 def solution(n, build_frame):
-    arr = []
-    gidung = [[0]*(n+1) for _ in range(n+1)]
-    bo = [[0]*(n+1) for _ in range(n+1)]
-    build = [[0]*(n+1) for _ in range(n+1)]
-    for x,y,a,b in build_frame:
-        if b == 1: # 설치
-            if a == 0: # 기둥
-                if (y == 0) or bo[x][y] == 1 or gidung[x][y] == 1:
-                    # build[x][y] += 2; build[x][y+1] += 2
-                    gidung[x][y] = 1; gidung[x][y+1] = 1
-                    arr.append([x,y,a])
-            elif a == 1: # 보
-                if (gidung[x+1][y] == 1 or gidung[x][y] == 1) or (bo[x+1][y] >= 1 and bo[x][y] == 1):
-                    bo[x + 1][y] = 1; bo[x][y] = 1
-                    arr.append([x, y, a])
-        # else: # 해제
-        #     if a == 0:  # 기둥
-        #         if build[x][y+1] >= 2 and build[x][y+1] >= 2:
-        #             build[x][y + 1] -= 2;  build[x][y] -= 2
-        #             # gidung[x][y] = 0
-        #             # gidung[x][y + 1] = 0
-        #             arr.remove([x,y,a])
-        #             # arr.remove([x, y+1, a])
-        #     elif a == 1: # 보
-        #         if build[x + 1][y] >= 3 and  build[x][y] >= 3:
-        #             build[x + 1][y] -= 1; build[x][y] -= 1
-        #             # bo[x+1][y] = 0
-        #             # bo[x][y] = 0
-        #             arr.remove([x, y, a])
-        #             # arr.remove([x + 1, y, a])
-    print("==================================")
-    for row in build:
-        print(row)
-    arr.sort()
-    print(arr)
-    # print("==================================")
-    # res = []
-    # for i in range(n+1):
-    #     for j in range(n+1):
-    #         if 0 <= bo[i][j] < n and 0 <= bo[i+1][j] < n:
-    #             if bo[i][j]:
-    #                 res.append([i,j,0])
-    #                 continue
-    #         if 0<= gidung[i][j+1] < n and 0<= gidung[i][j+1]< n:
-    #             if
-    #     print(row)
-    # print("==================================")
-    # for row in bo:
-    #     print(row)
-    # arr.sort()
-    # print(arr)
-    return arr
+    return 0
 
 n = 5
 # build_frame = [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]
--- Programmers/외벽점검.py
+++ Programmers/외벽점검.py
@@ -3,62 +3,59 @@
 취약한 지점들을 확인
 점검시간을 1시간으로 제한
 최소한의 친구들을 투입하여 취약지점 점검
+
+3 4 13 23 24
 '''
-# def comb(dist,m,r,tr,comb_list):
-#     if r == 0:
-#         comb_list.append(tr[:])
-#     elif r > m:
-#         return
-#     else:
-#         tr[r-1] = dist[m-1]
-#         comb(dist, m-1, r-1, tr,comb_list)
-#         comb(dist, m-1, r, tr,comb_list)
-#     return comb_list
-# def find(weak,comb_list):
-#     cnt = len(weak)
-#     print(comb_list)
-#     # while cnt:
-#     #     cnt -= 1
-#     #     inter_distance = []
-#     #
-#     # for row in comb_list:
-#     #     print(r)
 
+import itertools
 
-def find(weak,dist,m):
-    print("=============================")
-    print(weak,dist)
-    L = len(weak)
-    k = 0
-    visitied = [0]*L
-    for i in range(m):
-        cur = dist[i]
-        print("k ==>", k)
-        for j in range(k,L-1):
-            print(cur, weak[j + 1] - weak[j])
-            if visitied[k]:
-                k += 1
-            if cur >= weak[j+1] - weak[j]:
-                cur -= (weak[j+1] - weak[j])
-                visitied[j] = visitied[j+1] = 1
-                k = j+1
-                print("cur ==>" , cur)
+def calc(candidate, row):
+    j, cnt = 0, 1
+    # dist는 인덱스가 아닌 for문으로 한개씩 던져주고
+    # weaks의 row는 인덱스로 접근한다.
+    # candidate = [3,5,7], row = [1,3,4,9,10]일때
+    for num in candidate:
+        res = 0
+        # res = 3 + row[0] 인 4로 시작해서 while문에서 4보다 큰 row 인덱스를 찾아낸다
+        res += (num + row[j])
+        while True:
+            if j == len(row)-1:
+                break
+            # 4 >= row[1], 3 / 4 >= row[2], 4 / 4 >= row[3], 9 가되어 break
+            if res >= row[j+1]:
+                j += 1
             else:
+                j+=1
+                cnt += 1
                 break
 
-            print(visitied)
-def solution(n, weak, dist):
-    answer = 0
-    m = len(dist)
-    dist = dist[::-1]
-    for i in range(m):
-        find(weak,dist,m)
-        num = weak.pop(0)
-        weak.append(n+num)
-    return answer
+    answer.append(cnt)
 
+def solution(n, weak, dist):
+    global answer
+    answer = []
+    # 순열을 통해 모든 dist 후보 생성
+    candidates = itertools.permutations(dist, len(dist))
+
+    weaks = []
+    tmp = weak[:]
+    # weak의 모든 숫자가 처음이 되도록 하는 배열 생성
+    # ex) n = 12일때, [1,5,6,10], [5,6,10,13], [6,10,13,17] ...
+    for i in range(1, len(weak)):
+        tmp = tmp[1:] + [tmp[0]+n]
+        weaks.append(tmp)
+
+    # 한개의 dist마다 weak의 모든 경우를 비교한다
+    for candidate in candidates:  #j
+        for row in weaks:   #i
+            calc(candidate, row)
+
+    if min(answer) > len(dist):
+        return -1
+
+    return min(answer)
 
 n = 12
-weak =[1, 5, 6, 10]
-dist = [1, 2, 3, 4]
+weak = [1,5,6,10]
+dist = [1,2,3,4]
 solution(n, weak, dist)
